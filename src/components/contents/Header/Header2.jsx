1bd6a688e65ec04d216c999d4126979ca447bd50
fauziapriana29 <fauzicoding@gmail.com>
2020-09-26 11:50:22 +0700

    fix hide element

--- src/components/contents/Header/Header2.jsx
+++ src/components/contents/Header/Header2.jsx
@@ -9,8 +9,8 @@ const Header2 = () => {
     return (
         <div className="header-cont" id="1">
             <div className="main-cont">
-                <div className="cont-1 wow slideInLeft" data-wow-duration="2s"></div>
-                <div className="cont-2 wow slideInRight" data-wow-duration="2s">
+                <div className="cont-1 wow slideInLeft"></div>
+                <div className="cont-2 wow slideInRight">
                         <h3 className="title-name">Fauzi Apriana Basri</h3>
                         <p className="title-programmer">Programmer JavaScript</p>
                         <h4 className="phone">Phone:</h4>
@@ -22,7 +22,7 @@ const Header2 = () => {
                         <h4 className="phone sect2">Date of Birth:</h4>
                         <p className="title-phone">29th April 1999</p>
                 </div>
-                <div className="cont-3 wow slideInUp" data-wow-duration="2s">
+                <div className="cont-3">
                 <a href="https://www.instagram.com/fauziaprianapnb/?hl=id" ><img src={instagram} alt="" srcset="" className="logo"/></a>
                     <a href="https://twitter.com/AprianaFauzi" ><img src={twitter} alt="" srcset=""  className="logo"/></a>
                     <a href="https://www.facebook.com/fauzi.apriana.5/" ><img src={facebook} alt="" srcset=""  className="logo"/></a>
--- src/components/contents/firtsIntro/intro.jsx
+++ src/components/contents/firtsIntro/intro.jsx
@@ -6,7 +6,7 @@ const Intro = () => {
         <div>
             <div className="jumbotron" id="2">
                 <div className="cont-desc">
-                    <h1 className="display-4">Hello! Im Fauzi</h1>
+                    <h1 className="display-4">Hi! Im Fauzi</h1>
                     <p className="lead">On this website I will introduce myself, as written above, my name is Fauzi Apriana Basri. In my family i'am the first child and I have four siblings. When viewed from my nature, I am a very optimistic person in doing something, and I am also always punctual in any case.
                     And some things that will be introduced further below, such as My experience, My education, and My skills</p>
                 </div>
